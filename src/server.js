dbf7e035f2fa8d6c0f7925af9da34b76321398ee
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-09-23 14:51:14 +0100

    enhancement: add user model

--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 
-const exampleRouter = require("./middlewareExample");
+const User = require("./users/model");
 
 const port = process.env.PORT || 5001;
 
 const app = express();
-
+const syncTables = ()=> {
+    User.sync();
+}
 app.use(express.json());
 
-app.use(exampleRouter);
+
 
 app.use("/health", (req, res) =>{
     res.status(200).json({message: "API is healthy"});
 });
 app.listen(port, () =>{
+    syncTables();
     console.log(`server is listening on port ${port}`);
 
-})
+});
---
+++ src/users/routes.js
@@ -0,0 +1,5 @@
+const {Router} = require("express");
+userRouter = Router();
+
+
+module.exports = userRouter;
